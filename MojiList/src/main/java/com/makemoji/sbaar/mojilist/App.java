4861ac8667e4b15d8e2adcfde8a71a4c4eb6e968
Scott Baar <orphanfrequently@gmail.com>
2016-04-08 19:43:18 -0700

    formatting

--- MojiList/src/main/java/com/makemoji/sbaar/mojilist/App.java
+++ MojiList/src/main/java/com/makemoji/sbaar/mojilist/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     public void onCreate(){
         super.onCreate();
         context=this;
-        Moji.initialize(this,"940ced93abf2ca4175a4a865b38f1009d8848a58");
+        Moji.initialize(this,"YOUR-SDK-KEY-HERE");
         MMKB.setShareMessage("link to your app goes here");
         //LeakCanary.install(this);
     }
